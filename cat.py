e6ad9c4e9fa79378f36fe0dd216fa0d7b44431c9
Cheng1112 <41120502+Cheng1112@users.noreply.github.com>
2018-07-22 10:47:24 +0800

    complete Json write

--- cat.py
+++ cat.py
@@ -1,9 +1,25 @@
+import json
 # read nootebooks
-nootebooks_1 = {}
-cells_1 = nootebooks_1['cells']
+nootebooks_1 = open('1!!!.ipynb')
+nootebooks_1_str = nootebooks_1.read()
+nootebooks_1_json = json.loads(nootebooks_1_str)
 
-nootebooks_2 = {}
-cells_2 = nootebooks_2['cells']
+print(nootebooks_1)
+
+cells_1 = nootebooks_1_json['cells']
+
+nootebooks_2 = open('2!!!.ipynb')
+nootebooks_2_str = nootebooks_2.read()
+nootebooks_2_json = json.loads(nootebooks_2_str)
+
+print(nootebooks_2)
+
+cells_2 = nootebooks_2_json['cells']
 
 # cat two nootebooks
 target_cells = cells_1 + cells_2
+
+target_json = json.dumps(target_cells)
+target = open('target_notebook,ipynb','w')
+target.write(target_json)
+
